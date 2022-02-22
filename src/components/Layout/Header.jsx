1f6605ac18852d0b436a15620bf0b87be9db7123
WalkingFrozenFish <chachin.ol@yandex.kz>
2022-02-23 01:43:19 +0600

    Создание приложения

---
+++ src/components/Layout/Header.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+// Импорт изображения
+import sushiImage from "../../assets/sushi.jpg";
+import styles from "./Header.module.css";
+import HeaderCartButton from './HeaderCartButton';
+
+const Header = (props) => {
+    return (
+        // Использование фрагментов дает лучшую семантику и чистый код
+        <React.Fragment>
+            <header className={styles.header}>
+                <h1>Япона Кухня</h1>
+                <HeaderCartButton />
+            </header>
+            <div className={styles["main-image"]}>
+                <img src={sushiImage} alt="Блюда японской кухни" />
+            </div>
+        </React.Fragment>
+    )
+}
+
+export default Header
---
+++ src/components/Meals/MealItem/MealItemForm.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from "./MealItemForm.module.css";
+
+const MealItemForm = () => {
+    return (
+        <div>MealItemForm</div>
+    )
+}
+
+export default MealItemForm
