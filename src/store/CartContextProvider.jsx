fc7842a75fe874fab57edd34f1e967e8469ea6ac
WalkingFrozenFish <chachin.ol@yandex.kz>
2022-03-12 19:43:07 +0600

    Добавление товара в корзину

--- src/store/CartContextProvider.jsx
+++ src/store/CartContextProvider.jsx
@@ -1,14 +1,44 @@
 import React from 'react'
 import CartContext from './cart-context'
+import { useReducer } from 'react';
+
+const defaultCartState = {
+    items: [],
+    totalAmount: 0,
+}
+
+const cartReducer = (state, action) => {
+    if (action.type === "ADD_ITEM") {
+        const updatedItems = state.items.concat(action.item);
+        const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+        return {
+            items: updatedItems,
+            totalAmount: updatedTotalAmount
+        };
+    }
+    return defaultCartState;
+}
 
 const CartContextProvider = (props) => {
-    const addItemHandler = (item) => { };
+    const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState);
 
-    const removeItemHandler = (item) => { };
+    const addItemHandler = (item) => {
+        dispatchCartAction({
+            type: "ADD_ITEM",
+            item: item,
+        })
+    };
+
+    const removeItemHandler = (id) => {
+        dispatchCartAction({
+            type: "REMOVE_ITEM",
+            id: id,
+        })
+    };
 
     const cartContext = {
-        items: [],
-        totalAmount: 0,
+        items: cartState.items,
+        totalAmount: cartState.totalAmount,
         addItem: addItemHandler,
         removeItem: removeItemHandler
     }
